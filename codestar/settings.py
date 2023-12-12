0909137aab2e4a303fc123d7063836b03b86eb0d
danieljcave <>
2023-12-12 19:04:20 +0000

    deploy commit

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['8000-danieljcave-ci-fs-blog-86xb3i56p2.us2.codeanyapp.com',
                  'ci-fs-blog-85135e11dc32.herokuapp.com']
